b6105473e97bf41c90c378d1f5fe420ad58e3ec0
IsiahSlater <isiahmslater@gmail.com>
2024-08-28 18:24:39 -0500

    The Beginning of a small project where I try to recreate the mechanics of different games I enjoy. First up is Bugsnax, a game I hold near and dear to my heart.

---
+++ player.gd
@@ -0,0 +1,55 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.5
+const JUMP_VELOCITY = 4.5
+var sens = .5
+
+@onready var camera := $Body/Neck/Camera3D
+@onready var neck := $Body/Neck
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			neck.rotate_y(-event.relative.x * .01 * sens)
+			camera.rotate_x(-event.relative.y * .01 * sens)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+		
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	
+	# Get Mouse Position
+	# Get vector from position to middle of screen
+	
+	# Rotate character based on Vector
+	
+	move_and_slide()
+	
